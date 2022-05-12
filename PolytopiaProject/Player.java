d9eb5985152c1041bf9ef478a206307177f2f80c
kktam1001 <75771852+kktam1001@users.noreply.github.com>
2022-05-12 10:59:21 -0700

    working on player class

--- PolytopiaProject/Player.java
+++ PolytopiaProject/Player.java
@@ -11,10 +11,35 @@ public class Player
     private static Troop[][] troopMap = new Troop[16][16];
     private ArrayList<City> cities;
     private TechTree tree;
+    private int stars;
     
     public Player()
     {
         cities = new ArrayList<City>();
         tree = new TechTree();
+        stars = 5;
+    }
+    
+    public void incStars(int num)
+    {
+        stars += num;
+    }
+    
+    public void startTurn()
+    {
+        for (City c : cities)
+        {
+            
+        }
+    }
+    
+    public int getStars()
+    {
+        return stars;
+    }
+    
+    public void decStars(int num)
+    {
+        stars -= num;
     }
 }
